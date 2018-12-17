5a6ada85c67dfb5b64d40ec8c93ac641df7251b0
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-17 18:58:39 +0900

    Add branches project

---
+++ branches/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    condition();
+}
+
+fn condition() {
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+
+    println!("The value of number is: {}", number);
+}
