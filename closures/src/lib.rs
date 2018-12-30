e357159df14a93e83411a32bf2435757909c887d
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-30 21:41:18 +0900

    Add closures project

---
+++ closures/src/lib.rs
@@ -0,0 +1,17 @@
+// extern crate closures;
+// use simulated_expensive_calculation;
+
+// use crate::closures;
+
+#[cfg(test)]
+mod tests {
+    // use super::simulated_expensive_calculation;
+    // use super::*;
+
+    // #[test]
+    // fn call_with_different_values() {
+    // // let mut c = Cacher::new(|a| a);
+    // // let v1 = c.value(1);
+    // // simulated_expensive_calculation(1);
+    // }
+}
