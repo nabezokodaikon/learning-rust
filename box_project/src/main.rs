2f0b247652fcf7076b1da5f84cc9af944b48da0a
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-01-03 02:05:30 +0900

    Add box_project

---
+++ box_project/src/main.rs
@@ -0,0 +1,29 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+use self::List::*;
+// use List::{Cons, Nil};
+
+fn main() {
+    {
+        let b = Box::new(5);
+        println!("b = {}", b);
+    }
+
+    {
+        // let list = Cons(1, Cons(2, Cons(3, Nil)));
+    }
+
+    {
+        let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    }
+}
