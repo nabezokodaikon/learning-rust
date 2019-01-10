9d837002f7887582459bc588afaa3be0eeadc4b0
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-01-10 21:52:34 +0900

    Add A advanced-lifetimes project

---
+++ advanced-lifetimes/src/lib.rs
@@ -0,0 +1,27 @@
+struct Context<'s>(&'s str);
+
+struct Parser<'c, 's: 'c> {
+    context: &'c Context<'s>,
+}
+
+impl<'c, 's> Parser<'c, 's> {
+    fn parse(&self) -> Result<(), &'s str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+}
+
+struct Ref<'a, T: 'a>(&'a T);
+
+struct StaticRef<T: 'static>(&'static T);
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
