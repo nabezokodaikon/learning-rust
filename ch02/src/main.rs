6b2a12e4b8a1015181a3cc39cf575ad3493c9e49
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-09 20:33:43 +0900

    Add ch2

---
+++ ch02/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let _logical: bool = true;
+
+    let _a: f64 = 1.0;
+    let _an = 5i32;
+
+    let _default_float = 3.0;
+    let _default_integer = 7;
+
+    let mut _mutable = 12;
+}
