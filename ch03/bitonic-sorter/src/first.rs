736257f44e14965d29b7312bafda341a13265b64
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-01-06 23:44:32 +0900

    Add ch03-03-06

--- ch03/bitonic-sorter/src/first.rs
+++ ch03/bitonic-sorter/src/first.rs
@@ -8,9 +8,19 @@ pub fn sort(x: &mut [u32], up: bool) {
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
-    unimplemented!();
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] < x[mid_point + 1]) {
+            x.swap(i, mid_point + i);
+        }
+    }
 }
