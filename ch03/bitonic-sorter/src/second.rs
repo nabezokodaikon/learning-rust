294c7c16982950a342efe8b0ab417e7c8e21b950
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-01-09 23:33:27 +0900

    Modify use to sortOrder

--- ch03/bitonic-sorter/src/second.rs
+++ ch03/bitonic-sorter/src/second.rs
@@ -1,18 +1,19 @@
 #[cfg(test)]
 mod tests {
     use super::sort;
+    use crate::SortOrder::*;
 
     #[test]
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, true);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, false);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
         assert_eq!(x, vec!(330, 110, 30, 21, 20, 11, 10, 4));
     }
 
@@ -28,7 +29,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, true);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
         assert_eq!(
             x,
             vec![
@@ -56,7 +57,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, false);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
         assert_eq!(
             x,
             vec![
@@ -72,6 +73,12 @@ mod tests {
         );
     }
 
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 31, 11];
+        assert!(sort(&mut x, &Ascending).is_err());
+    }
+
     // #[test]
     // fn sort_f64() {
     // let mut x = vec![20.0, -30.0, 11.0, 10.0];
@@ -79,11 +86,28 @@ mod tests {
     // }
 }
 
-pub fn sort<T: Ord>(x: &mut [T], up: bool) {
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!(
+            "The length of x is not a power of two. (x.len(): {})",
+            x.len()
+        ))
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up);
     }
 }
