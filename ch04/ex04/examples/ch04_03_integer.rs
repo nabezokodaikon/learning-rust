5d78ea99a20e80e08d649e5e6d6c201c5d6b4685
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-01-20 00:07:58 +0900

    Add ch04_03_integer

--- ch04/ex04/examples/ch04_03_integer.rs
+++ ch04/ex04/examples/ch04_03_integer.rs
@@ -5,4 +5,11 @@ fn main() {
 
     let n4 = 10;
     let n5 = n3 + n4;
+
+    let h1 = 0xff;
+    let o1 = 0o744;
+    let b1 = 0b1010_0110_1110_1001;
+
+    let n6 = b'A';
+    assert_eq!(n6, 65u8);
 }
