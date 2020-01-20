8942184963b0fc29c44002df486df8231ce33dc1
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-01-20 23:30:35 +0900

    Add ch04_10_raw_pointer

---
+++ ch04/ex04/examples/ch04_10_raw_pointer.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let c1 = 'A';
+    let c1_ptr: *const char = &c1;
+    assert_eq!(unsafe { *c1_ptr }, 'A');
+
+    let mut n1 = 0;
+    let n1_ptr: *mut i32 = &mut n1;
+    assert_eq!(unsafe { *n1_ptr }, 0);
+
+    unsafe {
+        *n1_ptr = 1_000;
+        assert_eq!(*n1_ptr, 1_000);
+    }
+}
