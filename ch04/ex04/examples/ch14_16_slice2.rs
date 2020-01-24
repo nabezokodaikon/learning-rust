52aadc3fd58f55dcf9b6b54294df635c1898c8b5
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-01-25 01:40:50 +0900

    Modify ch14_16_slice2

--- ch04/ex04/examples/ch14_16_slice2.rs
+++ ch04/ex04/examples/ch14_16_slice2.rs
@@ -39,4 +39,63 @@ fn main() {
 
     s4b.sort_unstable();
     assert_eq!(s4b, &[1, 3, 4, 5, 7, 7, 9]);
+
+    let fruits = "あかりんご, あおりんご\nラズベリー, ブラックベリー";
+    let mut lines = fruits.lines();
+    let apple_line = lines.next();
+    assert_eq!(apple_line, Some("あかりんご, あおりんご"));
+    assert_eq!(lines.next(), Some("ラズベリー, ブラックベリー"));
+    assert_eq!(lines.next(), None);
+
+    if let Some(apples) = apple_line {
+        assert!(apples.starts_with("あか"));
+        assert!(apples.contains("りんご"));
+        assert_eq!(apples.find("あお"), Some(17));
+
+        let mut apple_iter = apples.split(",");
+        assert_eq!(apple_iter.next(), Some("あかりんご"));
+
+        let green = apple_iter.next();
+        assert_eq!(green, Some(" あおりんご"));
+        assert_eq!(green.map(str::trim), Some("あおりんご"));
+
+        assert_eq!(apple_iter.next(), None);
+    } else {
+        unreachable!();
+    }
+
+    let s = "abcあいう";
+    assert_eq!(s.get(0..1), Some("a"));
+    assert_eq!(s.get(3..6), Some("あ"));
+    assert_eq!(s.get(3..4), None);
+
+    let s = "かか\u{3099}く";
+    println!("{}", s);
+
+    let mut iter = s.chars();
+    assert_eq!(iter.next(), Some('か'));
+    assert_eq!(iter.next(), Some('か'));
+    assert_eq!(iter.next(), Some('\u{3099}'));
+    assert_eq!(iter.next(), Some('く'));
+    assert_eq!(iter.next(), None);
+
+    let utf8: [u8; 4] = [0x61, 0xe3, 0x81, 0x82];
+    assert_eq!(std::str::from_utf8(&utf8), Ok("aあ"));
+
+    let bad_utf8: [u8; 2] = [0x81, 0x33];
+    let result2 = std::str::from_utf8(&bad_utf8);
+    assert!(result2.is_err());
+    println!("{:?}", result2);
+
+    let mut s1 = "abcあいう".to_string();
+    let s2 = s1.as_mut_str();
+    s2.make_ascii_uppercase();
+    assert_eq!(s2, "ABCあいう");
+
+    let b = unsafe { s2.as_bytes_mut() };
+    b[3] = b'*';
+    b[4] = b'a';
+    b[5] = b'*';
+
+    assert_eq!(s1, "ABC*a*いう");
 }
