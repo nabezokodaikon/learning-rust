10b041bd7e0923783981660a1e000fee824412bf
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-08 00:30:26 +0900

    Add ch07_06

--- ch07/ex07/examples/ch07_02_move_semantics.rs
+++ ch07/ex07/examples/ch07_02_move_semantics.rs
@@ -8,4 +8,17 @@ fn main() {
     // println!("p1: {:?}", p1);
     p1 = scope::Parent(2, scope::Child(21), scope::Child(22));
     println!("p1: {:?}", p1);
+
+    let mut p1 = scope::Parent(1, scope::Child(11), scope::Child(12));
+    f1(&p1);
+    f2(&mut p1);
+    println!("p: {:?}", p1);
+}
+
+fn f1(p: &scope::Parent) {
+    println!("p: {:?}", p);
+}
+
+fn f2(p: &mut scope::Parent) {
+    p.0 += 1;
 }
