f5b765ff4884210261f7841587a028cd139b07cf
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-09 01:36:38 +0900

    Add toy_vec_02

--- ch07/toy-vec/examples/toy_vec_02.rs
+++ ch07/toy-vec/examples/toy_vec_02.rs
@@ -7,8 +7,28 @@ fn main() {
         v.push("Java Finch".to_string());
         v.push("Budgerigar".to_string());
 
-        e = v.get(1);
+        // e = v.get(1);
+        // println!("{}", e.is_some());
+
+        // match v.pop() {
+        // Some(a) => println!("{}", a),
+        // None => println!("None"),
+        // }
+        // match v.pop() {
+        // Some(a) => println!("{}", a),
+        // None => println!("None"),
+        // }
+        // match v.pop() {
+        // Some(a) => println!("{}", a),
+        // None => println!("None"),
+        // }
     }
 
-    assert_eq!(e, Some(&"Budgerigar".to_string()));
+    // assert_eq!(e, Some(&"Budgerigar".to_string()));
+
+    let mut v: ToyVec<usize> = ToyVec::new();
+    v.push(100);
+    let e = v.get(0);
+    // v.push(200);
+    assert_eq!(e, Some(&100));
 }
