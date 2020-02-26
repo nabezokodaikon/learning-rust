c72e8e9c0fc0afd4c50f7e3f810c722e8e518380
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-26 20:34:39 +0900

    Add P393

--- ch09/parser/src/main.rs
+++ ch09/parser/src/main.rs
@@ -186,6 +186,32 @@ fn test_lexer() {
     )
 }
 
+use std::io;
+
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+
+        if let Some(Ok(line)) = lines.next() {
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
 }
