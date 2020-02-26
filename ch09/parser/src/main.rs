cfb184c6d84aa870864fd9b000b5ec1f506b1860
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-26 21:10:55 +0900

    Add 9-3-2

--- ch09/parser/src/main.rs
+++ ch09/parser/src/main.rs
@@ -215,3 +215,101 @@ fn main() {
         }
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum AstKind {
+    /// 数値
+    Num(u64),
+    /// 単項演算
+    UniOp { op: UniOp, e: Box<Ast> },
+    /// 二項演算
+    BinOp { op: BinOp, l: Box<Ast>, r: Box<Ast> },
+}
+
+type Ast = Annot<AstKind>;
+
+impl Ast {
+    fn num(n: u64, loc: Loc) -> Self {
+        Self::new(AstKind::Num(n), loc)
+    }
+
+    fn uniop(op: UniOp, e: Ast, loc: Loc) -> Self {
+        Self::new(AstKind::UniOp { op, e: Box::new(e) }, loc)
+    }
+
+    fn binop(op: BinOp, l: Ast, r: Ast, loc: Loc) -> Self {
+        Self::new(
+            AstKind::BinOp {
+                op,
+                l: Box::new(l),
+                r: Box::new(r),
+            },
+            loc,
+        )
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum UniOpKind {
+    /// 正号
+    Plus,
+    /// 負号
+    Minus,
+}
+
+type UniOp = Annot<UniOpKind>;
+
+impl UniOp {
+    fn plus(loc: Loc) -> Self {
+        Self::new(UniOpKind::Plus, loc)
+    }
+
+    fn minus(loc: Loc) -> Self {
+        Self::new(UniOpKind::Minus, loc)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum BinOpKind {
+    /// 加算
+    Add,
+    /// 減算
+    Sub,
+    /// 乗算
+    Mult,
+    /// 除算
+    Div,
+}
+
+type BinOp = Annot<BinOpKind>;
+
+impl BinOp {
+    fn add(loc: Loc) -> Self {
+        Self::new(BinOpKind::Add, loc)
+    }
+    fn sub(loc: Loc) -> Self {
+        Self::new(BinOpKind::Sub, loc)
+    }
+    fn mult(loc: Loc) -> Self {
+        Self::new(BinOpKind::Mult, loc)
+    }
+    fn div(loc: Loc) -> Self {
+        Self::new(BinOpKind::Div, loc)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum ParseError {
+    /// 予期しないトークンがきた
+    UnexpectedToken(Token),
+    /// 四季を期待していたのに式でないものがきた
+    NotExpression(Token),
+    /// 演算子を期待していたのに演算子でないものがきた
+    NotOperator(Token),
+    /// 括弧が閉じられていない
+    UnclosedOpenParen(Token),
+    /// 式の解析が終わったのにまだトークンが残っている
+    RedundanExpression(Token),
+    /// パース途中で入力がおわった
+    Eof,
+}
