3b47fbb83b4e2a3351de2572c0a82ee05c1248cd
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-02-29 21:44:47 +0900

    Add 9-4-1

--- ch09/parser/src/main.rs
+++ ch09/parser/src/main.rs
@@ -198,6 +198,7 @@ fn prompt(s: &str) -> io::Result<()> {
 
 fn main() {
     use std::io::{stdin, BufRead, BufReader};
+    let mut compiler = RpnCompiler::new();
 
     let mut interp = Interpreter::new();
 
@@ -218,16 +219,18 @@ fn main() {
                 }
             };
 
-            let n = match interp.eval(&ast) {
-                Ok(n) => n,
-                Err(e) => {
-                    e.show_diagnostic(&line);
-                    show_trace(e);
-                    continue;
-                }
-            };
+            // let n = match interp.eval(&ast) {
+            // Ok(n) => n,
+            // Err(e) => {
+            // e.show_diagnostic(&line);
+            // show_trace(e);
+            // continue;
+            // }
+            // };
+            // println!("{}", n);
 
-            println!("{}", n);
+            let rpn = compiler.compile(&ast);
+            println!("{}", rpn);
         } else {
             break;
         }
@@ -731,6 +734,59 @@ impl StdError for InterpreterError {
         use self::InterpreterErrorKind::*;
         match self.value {
             DivisionByZero => "the right hand expression of the division evaluates to zero",
+        }
+    }
+}
+
+struct RpnCompiler;
+
+impl RpnCompiler {
+    pub fn new() -> Self {
+        RpnCompiler
+    }
+
+    pub fn compile(&mut self, expr: &Ast) -> String {
+        let mut buf = String::new();
+        self.compile_inner(expr, &mut buf);
+        buf
+    }
+
+    pub fn compile_inner(&mut self, expr: &Ast, buf: &mut String) {
+        use self::AstKind::*;
+        match expr.value {
+            Num(n) => buf.push_str(&n.to_string()),
+            UniOp { ref op, ref e } => {
+                self.compile_uniop(op, buf);
+                self.compile_inner(e, buf)
+            }
+            BinOp {
+                ref op,
+                ref l,
+                ref r,
+            } => {
+                self.compile_inner(l, buf);
+                buf.push_str(" ");
+                self.compile_inner(r, buf);
+                buf.push_str(" ");
+                self.compile_binop(op, buf)
+            }
+        }
+    }
+
+    fn compile_uniop(&mut self, op: &UniOp, buf: &mut String) {
+        use self::UniOpKind::*;
+        match op.value {
+            Plus => buf.push_str("+"),
+            Minus => buf.push_str("-"),
+        }
+    }
+    fn compile_binop(&mut self, op: &BinOp, buf: &mut String) {
+        use self::BinOpKind::*;
+        match op.value {
+            Add => buf.push_str("+"),
+            Sub => buf.push_str("-"),
+            Mult => buf.push_str("*"),
+            Div => buf.push_str("/"),
         }
     }
 }
