2c9ad1630c7dbe45ec5c47c08d53692ae59a4e1c
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-21 01:07:09 +0900

    Add code 4-21

--- chapter02/hello/src/main.rs
+++ chapter02/hello/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    let s = "hello rust world.";
+
+    let a = &s[..];
+    println!("a is {}", a);
+    let n = s.len();
+    let a = &s[0..n];
+    println!("a is {}", a);
+}
