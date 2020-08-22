c7247d6d6ded1b5e23d1cd7a5a1d10bb4eba25bf
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-23 01:44:29 +0900

    Chapter 7.2 completed

--- chapter02/hello/src/main.rs
+++ chapter02/hello/src/main.rs
@@ -1,23 +1,9 @@
-#[derive(Debug)]
-enum LANG {
-    JAPANESE = 81,
-    ENGLISH = 44,
-    CHINESE = 86,
-    FRANCH = 33,
-}
-
-enum Option<T> {
-    Some(T),
-    None,
-}
-
 fn main() {
-    let x = 'C';
-    let m = match x {
-        'A' => 1,
-        'B' => 2,
-        'C' => 3,
-        _ => -1,
-    };
-    println!("m is {}", m);
+    let mut v = vec!["one", "two", "three", "four", "five"];
+    v.sort();
+    let x = v.join(" ");
+    println!("x is {}", x);
+    v.reverse();
+    let x = v.join(" ");
+    println!("x is {}", x);
 }
