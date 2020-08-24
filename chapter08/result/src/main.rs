53b06a7ac9462a6a0db8f2001118d738b67563ea
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-25 07:23:19 +0900

    Chapter 8 completed

--- chapter08/result/src/main.rs
+++ chapter08/result/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = "sample.txt";
+    let data = std::fs::read_to_string(path)?;
+    println!("data is {}", data);
+    Ok(())
 }
