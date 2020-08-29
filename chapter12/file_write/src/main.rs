6264934ef1a31c52b1bd1de1a8a4f274a38c7062
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-30 01:07:55 +0900

    Fix chapter12

--- chapter12/file_write/src/main.rs
+++ chapter12/file_write/src/main.rs
@@ -1,9 +1,16 @@
+use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufReader, BufWriter, Read, Write};
 
-fn main() -> std::io::Result<()> {
-    let path = "sample6.txt";
-    let mut file = File::create(path)?;
-    file.write(b"hello rust world.\n")?;
-    Ok(())
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let file = File::open(&args[1]).expect("file not found");
+    let reader = BufReader::new(file);
+    let mut writer = BufWriter::new(std::io::stdout());
+    for it in reader.bytes() {
+        match it {
+            Ok(result) => writer.write(&[result]),
+            Err(_) => panic!(),
+        };
+    }
 }
