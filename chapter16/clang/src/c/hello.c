f6b15de4af5c04906d3150e63e4ed862cf98f900
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-31 15:21:54 +0900

    Completed chapter16.1

---
+++ chapter16/clang/src/c/hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void hello() {
+    printf("hello C world.\n");
+}
+
+int c_add(int x, int y) {
+    return x + y;
+}
---
+++ chapter16/clang/src/main.rs
@@ -0,0 +1,19 @@
+#[link(name = "hello", kind = "static")]
+extern "C" {
+    fn hello();
+}
+
+extern "C" {
+    fn c_add(a: i32, b: i32) -> i32;
+}
+
+fn main() {
+    unsafe {
+        hello();
+    }
+
+    let a = 10;
+    let b = 20;
+    let ans = unsafe { c_add(a, b) };
+    println!("ans is {}", ans);
+}
