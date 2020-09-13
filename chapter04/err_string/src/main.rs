320b2e589c6e491d18c490de9963e72ab151a72c
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-09-14 00:25:58 +0900

    Completed chapter4-4

--- chapter04/err_string/src/main.rs
+++ chapter04/err_string/src/main.rs
@@ -1,27 +1,121 @@
 use anyhow::{bail, ensure, Context, Result};
 
-fn get_int_from_file() -> Result<i32> {
-    let path = "number.txt";
+use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
-    let num_str = std::fs::read_to_string(path)
-        .with_context(|| format!("failed to read string from {}", path))?;
+struct RpnCalculator(bool);
 
-    if num_str.len() >= 10 {
-        bail!("it may be too large number");
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
     }
 
-    ensure!(num_str.starts_with("1"), "first digit is not 1");
+    pub fn eval(&self, formula: &str) -> Result<i32> {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
 
-    num_str
-        .trim()
-        .parse::<i32>()
-        .map(|t| t * 2)
-        .context("failed to parse string")
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
+        let mut stack = Vec::new();
+        let mut pos = 0;
+
+        while let Some(token) = tokens.pop() {
+            pos += 1;
+
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => bail!("invalid token at {}", pos),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        ensure!(stack.len() == 1, "invalid syntax");
+
+        Ok(stack[0])
+    }
 }
 
-fn main() {
-    match get_int_from_file() {
-        Ok(x) => println!("{}", x),
-        Err(e) => println!("{:#?}", e),
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path)?;
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose)
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose)
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
+    let calc = RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
+        let line = line?;
+        match calc.eval(&line) {
+            Ok(answer) => println!("{}", answer),
+            Err(e) => println!("{:#?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        let calc = RpnCalculator::new(false);
+        assert_eq!(calc.eval("5").unwrap(), 5);
+        assert_eq!(calc.eval("50").unwrap(), 50);
+        assert_eq!(calc.eval("-50").unwrap(), -50);
+
+        assert_eq!(calc.eval("2 3 +").unwrap(), 5);
+        assert_eq!(calc.eval("2 3 *").unwrap(), 6);
+        assert_eq!(calc.eval("2 3 -").unwrap(), -1);
+        assert_eq!(calc.eval("2 3 /").unwrap(), 0);
+        assert_eq!(calc.eval("2 3 %").unwrap(), 2);
+    }
+
+    #[test]
+    fn test_ng() {
+        let calc = RpnCalculator(false);
+        assert!(calc.eval("").is_err());
+        assert!(calc.eval("1 1 1 +").is_err());
+        assert!(calc.eval("+ 1 1").is_err());
     }
 }
