c9a1dcfeaea36b7838a801e52702d750986d3f8d
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-09-25 00:18:18 +0900

    Add

--- chapter06/mandelbrot/src/lib.rs
+++ chapter06/mandelbrot/src/lib.rs
@@ -1,5 +1,8 @@
 mod logic;
 mod utils;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::ImageData;
 
 use wasm_bindgen::prelude::*;
 
@@ -14,7 +17,30 @@ extern "C" {
     fn alert(s: &str);
 }
 
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mandelbrot!");
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+macro_rules! measure_elapsed_time {
+    ($t:tt, $s:block) => {{
+        let window = web_sys::window().expect("should have a window in this context");
+        let performance = window
+            .performance()
+            .expect("performance should be available");
+        let start = performance.now();
+        let result = { &s };
+        let end = performance.now();
+        console_log!("{}:{}[ms]", $t, end - start);
+        result
+    }};
 }
