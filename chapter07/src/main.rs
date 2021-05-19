717f9bef42d0b85dcd593d16d010ff631fba098b
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-05-19 10:23:17 +0900

    Completed chapter08

--- chapter07/src/main.rs
+++ chapter07/src/main.rs
@@ -91,12 +91,19 @@ fn main() {
     {
         let four = IpAddrKind::V4;
         let six = IpAddrKind::V6;
+        let home = IpAddr::V4(127, 0, 0, 1);
+        let loopback = IpAddr::V6(String::from("::1"));
     }
 }
 
 enum IpAddrKind {
     V4,
     V6,
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 struct Point2 {
